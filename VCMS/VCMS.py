024797c6dbd6b3283da420a1a3a1c07973685178
Ivan A <bladee.tr@yandex.ru>
2024-04-16 02:52:32 -0700

    HW4 solved

---
+++ VCMS/VCMS.py
@@ -0,0 +1,13 @@
+from sklearn.utils import resample
+from
+
+class VCMS:
+
+    def __init__(self, DP, m=10, b_1 = 100, b_2 = 100):
+        self.b_1 = b_1
+        self.b_2 = b_2
+        self.DP = DP
+        self.m = m
+
+
+    def get 
